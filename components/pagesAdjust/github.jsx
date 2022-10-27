1fc81b3d152246b5c89649e4afaeec6b76cb9b06
MadsonAlan <madson99hitech@gmail.com>
2022-10-26 22:50:29 -0300

    remoção da página github

--- components/pagesAdjust/github.jsx
+++ components/pagesAdjust/github.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import GitHubCalendar from 'react-github-calendar';
-import RepoCard from '../components/RepoCard';
+import RepoCard from '../RepoCard';
 import styles from '../styles/GithubPage.module.css';
 
 const GithubPage = ({ repos, user }) => {
