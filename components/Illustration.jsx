4296ae632f69390ff6cb32584352aa0afe319a0e
MadsonAlan <madson99hitech@gmail.com>
2022-10-26 22:44:48 -0300

    inicio da orgamização da adaptação do portifólio

---
+++ components/Illustration.jsx
@@ -0,0 +1,8 @@
+
+function Illustration(props) {
+  return (
+    <img align="right" src="https://raw.githubusercontent.com/gist/MadsonAlan/c4105defb52ee4655fb93e42c0ea9657/raw/dd74538815b6653d4e68258e9283376336634122/cardNLWCopa.svg"/>
+  );
+}
+
+export default Illustration;
---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  images: {
+    domains: [
+      'gist.github.com',
+      'res.cloudinary.com',
+      'avatars.githubusercontent.com',
+      'imgur.com',
+    ],
+  },
+};
